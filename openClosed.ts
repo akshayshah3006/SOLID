1521dda6803cfc7bb70edab00b957755eb6a4f05
Bacancy <akshay.shah@bacancy.com>
2023-12-21 13:48:14 +0530

    added new Shape Circle

--- openClosed.ts
+++ openClosed.ts
@@ -25,20 +25,33 @@ class Rectangle1 implements Shape {
   }
 }
 
+class Circle implements Shape {
+  private radius: number;
+  constructor(radius: number) {
+    this.radius = radius;
+  }
+  public getArea(): number {
+    return 3.14 * this.radius * this.radius;
+  }
+}
+
 class Print1 {
   private shape: Shape;
   constructor(shape: Shape) {
     this.shape = shape;
   }
   public printObject(): void {
-    console.log("Area of Square", this.shape.getArea());
+    console.log("Area is", this.shape.getArea());
   }
 }
 
 const rectArea1 = new Rectangle1(10, 12);
 const sqArea1 = new Square1(12);
+const circArea = new Circle(10);
 
 const t1 = new Print1(rectArea1);
 const j1 = new Print1(sqArea1);
+const k1 = new Print1(circArea);
 t1.printObject();
 j1.printObject();
+k1.printObject();
